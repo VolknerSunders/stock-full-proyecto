7390823ab7cc1386a0fc2b21235267ada0643e3f
VolknerSunders <volknereleck@gmail.com>
2022-11-21 19:55:19 -0700

    se cambio el contexto de categoria y de usuario a por reducers, se agrego la libreria de reselect

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from 'react';
-import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductCard from '../../components/product-card/product-card.component';
 import { Fragment } from 'react';
-
-import { CategoriesContext } from '../../contexts/categories.context';
+import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { useSelector } from 'react-redux';
 import './category.styles.scss'
 
 const Category = () => {
     const {category} = useParams();
-    const { categoriesMap } = useContext(CategoriesContext);
+    const categoriesMap = useSelector(selectCategoriesMap);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,12 @@
 import { Fragment, useContext } from "react"
-import { Outlet, Link } from "react-router-dom"
+import { Outlet } from "react-router-dom"
+import { useSelector } from "react-redux";
 
 import { ReactComponent as Logo} from '../../assets/logo.svg'
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
-import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
@@ -14,7 +14,7 @@ import { signOutUser } from "../../utils/firebase/firebase.utils";
 import { NavigationContainer,LogoContainer,NavLinks,NavLink } from "./navigation.styles";
 
 const Navigation = () =>{
-    const { currentUser } = useContext(UserContext);
+    const currentUser = useSelector((state) => state.user.currentUser);
     const { isCartOpen} = useContext(CartContext);
 
     return (
